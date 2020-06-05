0814b2949f009127deeb2aa29ee1313062f404a3
Ambrish Rawat <ambrish.rawat@ie.ibm.com>
2020-06-05 10:19:49 +0100

    tenorflowv2 work in pogress
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- art/defences/trainer/__init__.py
+++ art/defences/trainer/__init__.py
@@ -5,3 +5,4 @@ from art.defences.trainer.trainer import Trainer
 from art.defences.trainer.adversarial_trainer import AdversarialTrainer
 from art.defences.trainer.adversarial_trainer_madry_pgd import AdversarialTrainerMadryPGD
 from art.defences.trainer.adversarial_trainer_FBF_Pytorch import AdversarialTrainerFBFPyTorch
+from art.defences.trainer.adversarial_trainer_FBF_tensorflowv2 import AdversarialTrainerFBFTensorflowv2
